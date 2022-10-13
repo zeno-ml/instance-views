673084407a275494001faf70eab3c450e371ba52
Alex Cabrera <alex.cabrera@gmail.com>
2022-10-12 20:21:23 -0400

    feat: image segmentation:

---
+++ image-segmentation/src/Options.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import SegmentedButton, { Label, Segment } from "@smui/segmented-button";
+
+  export let setOptions;
+
+  let choices = ["Label", "Model"];
+  let selected = ["Label"];
+
+  $: setOptions({ mask: selected });
+  $: console.log(selected);
+</script>
+
+<div style:margin-left="10px">
+  <SegmentedButton segments={choices} let:segment bind:selected>
+    <Segment {segment}>
+      <Label>{segment}</Label>
+    </Segment>
+  </SegmentedButton>
+  <button on:click={() => console.log("hi")}>click</button>
+</div>
